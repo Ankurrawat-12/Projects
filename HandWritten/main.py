d839ce10a09c2554c82b0d7f6c73c0d6f44e6852
Ankur Rawat <ankurrawat620@gmail.com>
2022-10-11 18:13:33 +0530

    HandWritten

---
+++ HandWritten/main.py
@@ -0,0 +1,14 @@
+import pywhatkit as py
+# py.text_to_handwriting("My name is ankur rawat i am 17 years old i am currently learning python and i am at"
+#                               "intermediate level at python. I love to watch anime all the time , my favourite anime"
+#                               "is naruto . i am currently watching one piece, my hero academia, "
+#                               "boruto, burning kabbadi, my brothers name is saurabh", rgb=(0, 0, 255))
+# phone_number = int(input("Enter the phone number :- "))
+# message = str(input("Enter your message :- "))
+# hour = int(input("Enter Hour :- "))
+# minute = int(input("Enter Minute :- "))
+#
+# py.sendwhatmsg(f"+91{phone_number}", f"{message}", time_hour=hour, time_min=minute)
+# py.info("Bleach")
+# py.image_to_ascii_art(r"C:\Users\ankur\Pictures\Naruto\portraitwp24.png",
+#                       r"C:\Users\ankur\Pictures\Naruto\pcwallpaper115.txt")
